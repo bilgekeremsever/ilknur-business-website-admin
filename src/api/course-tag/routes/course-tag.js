7c8e8e135aa048589723ff4d0dbb60c9e7322c5d
Bilge <bilgekerems@gmail.com>
2022-03-19 21:04:22 +0300

    Add course & course tag schema.

---
+++ src/api/course-tag/routes/course-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * course-tag router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::course-tag.course-tag');
---
+++ src/api/course-tag/services/course-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * course-tag service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::course-tag.course-tag');
